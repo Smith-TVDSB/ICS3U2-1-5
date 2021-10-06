83c2fc61006a0f68dab97116d0e1555986a27988
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2021-10-05 20:05:16 -0400

    Initial commit

---
+++ student.hpp
@@ -0,0 +1,16 @@
+#pragma once // prevents multiple definitions
+
+#include <iostream> // print library
+#include <string>
+using namespace std;
+
+// ===============
+// implementations
+
+//Exercise
+bool areOdd(int a, int b)
+{
+	return 0; // TODO: fix
+}
+
+
